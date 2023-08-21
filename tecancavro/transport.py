bfafb98ae9517813da3d8a19ffecb675a8d8e363
jerry-dellatech <dr.jerry.l.martin@gmail.com>
2023-08-20 20:56:04 -0600

    Progress toward micropython rp2040 port
    Making progress toward port to Pi Pico (RP2040). Stripped out code for windows. Implement serial communications using UART. Code runs to point of trying to communicate with pump, but RS232 is not yet implemented in hardware. Note that code requires contextlib, and functools to run. These have to be installed on microcontroller using Thonny.

--- tecancavro/transport.py
+++ tecancavro/transport.py
@@ -13,17 +13,22 @@ which sends a command string (`cmd`) and returns a dictionary containing the
 
 """
 
-import glob
+# import glob
 import sys
-import uuid
+# import uuid
 import time
+from random import getrandbits # used as alternative to uuid
 
-import serial
+# import serial
 
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
+if sys.platform.startswith('rp2'):
+        # Pi pico or similar
+        from machine import UART
+         
+# try:
+#     import urllib.request as urllib2
+# except ImportError:
+#     import urllib2
 
 try:
     import simplejson as json
@@ -53,36 +58,33 @@ def listSerialPorts():
 
     elif sys.platform.startswith('rp2'):
         # Pi pico or similar
-        from machine import UART
-         
-        ports = ['uart0','uart1']
-
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this is to exclude your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
+        ports = [0] # could also include uart1 if needed
 
     else:
         raise EnvironmentError('Unsupported platform')
 
     result = []
-    for port in ports:
-        try:
-            # print(f'checking port {port}')
-            s = serial.Serial(port)
-            s.close()
+
+    if sys.platform.startswith('rp2'):
+        for port in ports:
             result.append(port)
-        except (OSError, serial.SerialException):
-            pass
+
+    else:
+        for port in ports:
+            try:
+                # print(f'checking port {port}')
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
     return result
 
-
-class TecanAPISerial(TecanAPI):
+class TecanAPIMicro(TecanAPI):
     """
     Wraps the TecanAPI class to provide serial communication encapsulation
-    and management for the Tecan OEM API. Maps devices to a state-monitored
+    and management for the Tecan OEM API. Designed to run on microcontrollers
+    like the Pi Pico (RP2040). Maps devices to a state-monitored
     dictionary, `ser_mapping`, which allows multiple Tecan devices to
     share a serial port (provided that the serial params are the same).
     """
@@ -90,9 +92,11 @@ class TecanAPISerial(TecanAPI):
     ser_mapping = {}
 
     @classmethod
-    def findSerialPumps(cls, tecan_addrs=[0], ser_baud=9600, ser_timeout=0.2,
+    def findSerialPumps(cls, tecan_addrs=[0], ser_baud=9600, ser_timeout=200,
                         max_attempts=2):
-        ''' Find any enumerated syringe pumps on the local com / serial ports.
+        ''' Find any enumerated syringe pumps on the serial ports.
+
+        timeout in ms.
 
         Returns list of (<ser_port>, <pump_config>, <pump_firmware_version>)
         tuples.
@@ -121,9 +125,10 @@ class TecanAPISerial(TecanAPI):
     def __init__(self, tecan_addr, ser_port, ser_baud, ser_timeout=0.1,
                  max_attempts=5):
 
-        super(TecanAPISerial, self).__init__(tecan_addr)
+        super(TecanAPIMicro, self).__init__(tecan_addr)
 
-        self.id_ = str(uuid.uuid4())
+        self.id_ = getrandbits(32) # poor mans uuid
+        # self.id_ = str(getrandbits(32)) # poor mans uuid
         self.ser_port = ser_port
         self.ser_info = {
             'baud': ser_baud,
@@ -146,7 +151,8 @@ class TecanAPISerial(TecanAPI):
                 if frame_in:
                     return frame_in
                 sleep(0.05 * attempt_num)
-            except serial.SerialException:
+            # except serial.SerialException:
+            except:
                 sleep(0.2)
         raise(TecanAPITimeout('Tecan serial communication exceeded max '
                               'attempts [{0}]'.format(
@@ -170,13 +176,15 @@ class TecanAPISerial(TecanAPI):
         see if the parameters match, and if they do, shares the connection.
         Otherwise it raises a `serial.SerialException`.
         """
-        reg = TecanAPISerial.ser_mapping
+        reg = TecanAPIMicro.ser_mapping
         port = self.ser_port
+        print(port)
         if self.ser_port not in reg:
             reg[port] = {}
             reg[port]['info'] = {k: v for k, v in self.ser_info.items()}
-            # TODO: change this to use machine.UART
-            reg[port]['_ser'] = serial.Serial(port=port,
+            print( reg[port]['info'])
+            uart = UART(port)
+            reg[port]['_ser'] = uart.init(
                                     baudrate=reg[port]['info']['baud'],
                                     timeout=reg[port]['info']['timeout'])
             reg[port]['_devices'] = [self.id_]
@@ -200,104 +208,134 @@ class TecanAPISerial(TecanAPI):
             ind = dev_list.index(self.id_)
             del dev_list[ind]
             if len(dev_list) == 0:
-                port_reg['_ser'].close()
+                port_reg['_ser'].deinit()
                 del port_reg, TecanAPISerial.ser_mapping[self.ser_port]
         except KeyError:
             pass
 
-'''
-class TecanAPINode(TecanAPI):
-    """
-    `TecanAPI` subclass for node-based serial bridge communication.
-    Tailored for the ARC GT sequencing platform.
-    """
+# class TecanAPISerial(TecanAPI):
+#     """
+#     Wraps the TecanAPI class to provide serial communication encapsulation
+#     and management for the Tecan OEM API. Maps devices to a state-monitored
+#     dictionary, `ser_mapping`, which allows multiple Tecan devices to
+#     share a serial port (provided that the serial params are the same).
+#     """
 
-    def __init__(self, tecan_addr, node_addr, response_len=20,
-                 max_attempts=5):
-        super(TecanAPINode, self).__init__(tecan_addr)
-        self.node_addr = node_addr
-        self.response_len = response_len
-        self.max_attempts = max_attempts
+#     ser_mapping = {}
 
-    def sendRcv(self, cmd):
-        attempt_num = 0
-        while attempt_num < self.max_attempts:
-            attempt_num += 1
-            if attempt_num == 1:
-                frame_out = self.emitFrame(cmd)
-            else:
-                frame_out = self.emitRepeat()
-            url = ('http://{0}/syringe?LENGTH={1}&SYRINGE={2}'
-                   ''.format(self.node_addr, self.response_len,
-                            frame_out))
-            raw_in = self._jsonFetch(url)
-            frame_in = self._analyzeFrame(raw_in)
-            if frame_in:
-                return frame_in
-            sleep(0.2 * attempt_num)
-        raise(TecanAPITimeout('Tecan HTTP communication exceeded max '
-                              'attempts [{0}]'.format(
-                              self.max_attempts)))
+#     @classmethod
+#     def findSerialPumps(cls, tecan_addrs=[0], ser_baud=9600, ser_timeout=0.2,
+#                         max_attempts=2):
+#         ''' Find any enumerated syringe pumps on the local com / serial ports.
 
-    #Override _buildFrame for hex encoding
-    def _buildFrame(self, repeat=False):
-        if repeat:
-            seq_byte = int('00111{}'.format(self.SEQ_NUM), 2)
-        else:
-            seq_byte = int('00110{}'.format(next(self.rotateSeqNum())), 2)
-        frame_list = [self.START_BYTE, self.addr, seq_byte] + \
-                      self._assembleCmd() + [self.STOP_BYTE]
-        checksum = self._buildChecksum(frame_list)
-        frame_list.append(checksum)
-        return ''.join( [ "%02X" % x for x in frame_list ] )
+#         Returns list of (<ser_port>, <pump_config>, <pump_firmware_version>)
+#         tuples.
+#         '''
+#         print('Starting findSerialPumps...')
+#         found_devices = []
+#         for port_path in listSerialPorts():
+#             # print(f'Checking port {port_path}')
+#             for addr in tecan_addrs:
+#                 # print(f'Checking address {addr}')
+               
+#                 try:
+#                     p = cls(addr, port_path, ser_baud,
+#                             ser_timeout, max_attempts)
+#                     config = p.sendRcv('?76')['data']
+#                     fw_version = p.sendRcv('&')['data']
+#                     found_devices.append((port_path, config, fw_version))
+#                 except OSError as e:
+#                     if e.errno != 16:  # Resource busy
+#                         raise
+#                 except TecanAPITimeout:
+#                     pass
+#         # print(f'devices found = {found_devices}')
+#         return found_devices
 
-    #Override _analyzeFrame for hex encoding
-    def _analyzeFrame(self, raw_packet):
-        data_str = raw_packet['MSG']
-        raw_frame = [data_str[i:i+2].decode('hex') for i in\
-                     range(0, len(data_str), 2)]
-        try:
-            # Get basic indices
-            frame = raw_frame[raw_frame.index(chr(self.START_BYTE)):
-                              raw_frame.index(chr(self.STOP_BYTE))+2]
-            if len(frame) < 5:
-                return False
-            frame_list = [byte for byte in frame]
-            int_list = [ord(byte) for byte in frame]
-            etx_idx = int_list.index(self.STOP_BYTE)
-            data_len = etx_idx - 3
-        except:
-            return False
-        # Integrity checks
-        if not frame_list[1] == '0':
-            # Master address is always 30h (ASCII 0)
-            return False
-        if not self._verifyChecksum(int_list):
-            return False
-        # Dump payload
-        if data_len != 0:
-            data = ''.join(frame_list[3:etx_idx])
-        else:
-            data = None
-        status_frame = bin(ord(frame_list[2]))[2:].zfill(8)
-        payload = {
-            'status_byte': status_frame,
-            'data': data
-        }
-        return payload
+#     def __init__(self, tecan_addr, ser_port, ser_baud, ser_timeout=0.1,
+#                  max_attempts=5):
 
-    def _jsonFetch(self, url):
-        fdurl = None
-        data = None
-        try:
-            fdurl = urllib2.urlopen(url)
-            data = fdurl.read()
-        finally:
-            if fdurl:
-                fdurl.fp._sock.fp._sock.close() # close the "real" socket later
-                fdurl.close()
-        if data:
-            return json.loads(data)
-        else:
-            return None
-'''
+#         super(TecanAPISerial, self).__init__(tecan_addr)
+
+#         self.id_ = str(uuid.uuid4())
+#         self.ser_port = ser_port
+#         self.ser_info = {
+#             'baud': ser_baud,
+#             'timeout': ser_timeout,
+#             'max_attempts': max_attempts
+#         }
+#         self._registerSer()
+
+#     def sendRcv(self, cmd):
+#         attempt_num = 0
+#         while attempt_num < self.ser_info['max_attempts']:
+#             try:
+#                 attempt_num += 1
+#                 if attempt_num == 1:
+#                     frame_out = self.emitFrame(cmd)
+#                 else:
+#                     frame_out = self.emitRepeat()
+#                 self._sendFrame(frame_out)
+#                 frame_in = self._receiveFrame()
+#                 if frame_in:
+#                     return frame_in
+#                 sleep(0.05 * attempt_num)
+#             except serial.SerialException:
+#                 sleep(0.2)
+#         raise(TecanAPITimeout('Tecan serial communication exceeded max '
+#                               'attempts [{0}]'.format(
+#                               self.ser_info['max_attempts'])))
+
+#     def _sendFrame(self, frame):
+#         self._ser.write(frame)
+
+#     def _receiveFrame(self):
+#         raw_data = b''
+#         raw_byte = self._ser.read()
+#         while raw_byte != b'':
+#             raw_data += raw_byte
+#             raw_byte = self._ser.read()
+#         return self.parseFrame(raw_data)
+
+#     def _registerSer(self):
+#         """
+#         Checks to see if another TecanAPISerial instance has registered the
+#         same serial port in `ser_mapping`. If there is a conflict, checks to
+#         see if the parameters match, and if they do, shares the connection.
+#         Otherwise it raises a `serial.SerialException`.
+#         """
+#         reg = TecanAPISerial.ser_mapping
+#         port = self.ser_port
+#         if self.ser_port not in reg:
+#             reg[port] = {}
+#             reg[port]['info'] = {k: v for k, v in self.ser_info.items()}
+#             # TODO: change this to use machine.UART
+#             reg[port]['_ser'] = serial.Serial(port=port,
+#                                     baudrate=reg[port]['info']['baud'],
+#                                     timeout=reg[port]['info']['timeout'])
+#             reg[port]['_devices'] = [self.id_]
+#         else:
+#             if len(set(self.ser_info.items()) &
+#                set(reg[port]['info'].items())) != 3:
+#                 raise serial.SerialException('TecanAPISerial conflict: ' \
+#                     'another device is already registered to {0} with ' \
+#                     'different parameters'.format(port))
+#             else:
+#                 reg[port]['_devices'].append(self.id_)
+#         self._ser = reg[port]['_ser']
+
+#     def __del__(self):
+#         """
+#         Cleanup serial port registration on delete
+#         """
+#         port_reg = TecanAPISerial.ser_mapping[self.ser_port]
+#         try:
+#             dev_list = port_reg['_devices']
+#             ind = dev_list.index(self.id_)
+#             del dev_list[ind]
+#             if len(dev_list) == 0:
+#                 port_reg['_ser'].close()
+#                 del port_reg, TecanAPISerial.ser_mapping[self.ser_port]
+#         except KeyError:
+#             pass
+
---
+++ testMicro.py
@@ -0,0 +1,39 @@
+from tecancavro.models import XCaliburD
+
+from tecancavro.transport import TecanAPIMicro
+
+# Functions to return instantiated XCaliburD objects for testing
+
+def returnSerialXCaliburD():
+    test0 = XCaliburD(com_link=TecanAPIMicro(0, '/dev/tty.usbserial', 9600))
+    return test0
+
+def returnNodeXCaliburD():
+	test0 = XCaliburD(com_link=TecanAPINode(0, '192.168.1.140:80'), waste_port=6)
+	return test0
+
+def findSerialPumps():
+    print("looking for pumps...")
+    return TecanAPIMicro.findSerialPumps()
+
+def getSerialPumps():
+    ''' Assumes that the pumps are XCaliburD pumps and returns a list of
+    (<serial port>, <instantiated XCaliburD>) tuples
+    '''
+    pump_list = findSerialPumps()
+    return [(ser_port, XCaliburD(com_link=TecanAPISerial(0,
+             ser_port, 9600))) for ser_port, _, _ in pump_list]
+
+
+if __name__ == '__main__':
+    # print(findSerialPumps())
+    pumps = getSerialPumps()
+    pumps_dict = dict(pumps)
+    print(pumps_dict)
+    pump1 = pumps_dict['COM13']
+    pump1.init(in_port=1, out_port=3)
+    pump1.extract(1,2000)
+    pump1.dispense(3,2000)
+    delay = pump1.executeChain()
+    pump1.waitReady(delay)
+    print("done")
